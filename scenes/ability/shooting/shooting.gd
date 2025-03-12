5a5214e4723ebbd68a6940108b203686dbcf6b03
Allandt Bik-Elliott <allandt@gmail.com>
2025-03-12 17:30:52 +0000

    remove log and use Input.get_vector instead of manually calculating vector

--- scenes/ability/shooting/shooting.gd
+++ scenes/ability/shooting/shooting.gd
@@ -9,7 +9,6 @@ func _draw():
 		return
 	var parent_pos = parent.global_position
 	var line_vector = shoot_vector.normalized() * RAY_LENGTH
-	print(line_vector)
 	draw_line(Vector2.ZERO, line_vector, Color.DARK_ORCHID, 1, false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,6 +16,5 @@ func _process(_delta):
 	queue_redraw()
 
 func get_shoot_vector():
-	var x_shoot = Input.get_action_strength("shoot_right") - Input.get_action_strength("shoot_left")
-	var y_shoot = Input.get_action_strength("shoot_down") - Input.get_action_strength("shoot_up")
-	return Vector2(x_shoot, y_shoot)
+	return Input.get_vector("shoot_left", "shoot_right", "shoot_up", "shoot_down")
+	
